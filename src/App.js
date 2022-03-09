14adb352edbfc1697aaf6b6a090424757110da48
josephvasconcelos <joseph.vasconcelos97@gmail.com>
2022-03-09 13:28:45 -0500

    changed title for web

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PathFinder from './PathFinder/PathFinder';
 function App() {
   return (
     <div className="App">
+      <p>Draw a wall by clicking on the grid, you can then visualize the path that the green block will find the target</p>
       <PathFinder></PathFinder>
     </div>
   );
